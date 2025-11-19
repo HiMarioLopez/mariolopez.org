7fa18b09431ae1a21bb7a3891ca6a88e6cbe5950
Mario Lopez Martinez <mario@lopez.fyi>
2025-11-18 23:43:21 -0600

    fix: safari not registering clicks for ascii text lightbulb

--- components/ui/ascii-text.tsx
+++ components/ui/ascii-text.tsx
@@ -165,6 +165,7 @@ class AsciiFilter {
     this.domElement.style.left = "0";
     this.domElement.style.width = "100%";
     this.domElement.style.height = "100%";
+    this.domElement.style.pointerEvents = "none";
 
     this.pre = document.createElement("pre");
     this.domElement.appendChild(this.pre);
